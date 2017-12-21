0bf26faffa2c540dd8dbdc4742ffc6716564f7bf
Drew Finelli <asfinelli@gmail.com>
2017-12-21 14:44:44 -0500

    doctor login

--- app/controllers/doctors_sessions_controller.rb
+++ app/controllers/doctors_sessions_controller.rb
@@ -19,7 +19,7 @@ class DoctorsSessionsController < ApplicationController
         @doctor = Doctor.find_by(username: params[:doctor][:username]).try(:authenticate, params[:doctor][:password])
         if @doctor
           session[:doctor_id] = @doctor.id
-          redirect_to doctors_sessions_path
+          redirect_to doctors_session_path(@doctor)
         else
           flash[:notice] = "Not a valid username.  Please sign up first!"
           redirect_to new_doctor_path
