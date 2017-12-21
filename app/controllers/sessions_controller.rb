72248a69567c9ea30f3d4aec50a818e921e2c6c6
Shable Harris <shableharris92@gmail.com>
2017-12-21 13:28:48 -0500

    minor changes to Gemfile and code styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,24 @@
 class SessionsController < ApplicationController
   def new
-        @user = User.new
-      end
-      def index
-        redirect_to new_session_path
-      end
+    @user = User.new
+  end
 
-      def create
-        @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
-        if @user
-          session[:user_id] = @user.id
-          redirect_to userdashboards_path
-        else
-          flash[:notice] = "Not a valid username.  Please sign up first!"
-          redirect_to new_user_path
-        end
-      end
+  def index
+    redirect_to new_session_path
+  end
 
-      def show
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
 
-      end
+    if @user
+      session[:user_id] = @user.id
+      redirect_to userdashboards_path
+    else
+      flash[:notice] = "Not a valid username.  Please sign up first!"
+      redirect_to new_user_path
+    end
+  end
+
+  def show
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,23 +2,25 @@ class UsersController < ApplicationController
   def index
       @users = User.all
     end
-    def new
-        @user = User.new
-      end
 
-      def create
-        @user = User.new(user_params)
-        if @user.save
-          redirect_to user_path, notice: 'Created User'
-        else
-          render :new
-        end
-      end
-      def show
+  def new
+    @user = User.new
+  end
 
-      end
-      private
-          def user_params
-            params.require(:user).permit(:username, :name, :dob, :phone_number, :email, :gender, :address, :city, :state, :postal_code, :country, :password, :password_confirmation)
-          end
+  def create
+  @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path, notice: 'Created User'
+    else
+      render :new
+    end
+  end
+  def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :dob, :phone_number, :email, :gender, :address, :city, :state, :postal_code, :country, :password, :password_confirmation)
+  end
 end
