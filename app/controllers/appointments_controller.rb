7a3765917918926b0a623c5865eb6098beeaf6ea
Drew Finelli <asfinelli@gmail.com>
2017-12-21 17:59:13 -0500

    user table

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,19 @@
 class AppointmentsController < ApplicationController
+  def index
+  end
+
   def create
   end
-  def show
+  def new
+    @appointment = Appointment.new(user: current_user)
+    if @appointment.save!
+      redirect_to appointments_path
+    else
+      render :new
+    end
+
+    def show
+      
+    end
   end
 end
