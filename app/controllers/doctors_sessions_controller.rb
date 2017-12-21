74275acd8f5aeb8c5038c68fefdfb292d01ae555
Claire Murray <claire.e.murray00@gmail.com>
2017-12-21 10:29:31 -0500

    adding views for doctors and doctors sessions

--- app/controllers/doctors_sessions_controller.rb
+++ app/controllers/doctors_sessions_controller.rb
@@ -1,2 +1,27 @@
 class DoctorsSessionsController < ApplicationController
+
+  def new
+        @doctor = Doctor.new
+      end
+
+      def index
+        redirect_to doctors_session_path
+
+
+      end
+
+      def show
+      end
+
+      def create
+        # byebug
+        @doctor = Doctor.find_by(username: params[:doctor][:username]).try(:authenticate, params[:doctor][:password])
+        if @doctor
+          session[:doctor_id] = @doctor.id
+          redirect_to doctors_sessions_path
+        else
+          flash[:notice] = "Not a valid username.  Please sign up first!"
+          redirect_to new_doctor_path
+        end
+      end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+        @user = User.new
+      end
+      def index
+        redirect_to new_session_path
+      end
+
+      def create
+        @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+        if @user
+          session[:user_id] = @user.id
+          redirect_to userdashboards_path
+        else
+          flash[:notice] = "Not a valid username.  Please sign up first!"
+          redirect_to new_user_path
+        end
+      end
+
+      def show
+
+      end
 end
