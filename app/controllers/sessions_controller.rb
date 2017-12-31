06117fac41b3979dceca96aa84a7cef3a995b8e7
Shable Harris <shableharris92@gmail.com>
2017-12-31 10:20:29 -0500

    -clean up pages -fixed doctor dashboard -added back button to appointment -moved sessions show to index, will use show for patient history -added dashboard to multiple html views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,23 @@
 class SessionsController < ApplicationController
- before_action :require_logged_in, only: [:show]
- def new
+before_action :require_logged_in, only: [:show]
+  def new
    @user = User.new
- end
- def create
+  end
+  def create
    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
    if @user
      session[:user_id] = @user.id
-     redirect_to session_path(@user)
+     redirect_to sessions_path(@user)
    else
      flash[:notice] = "Not a valid username.  Please sign up first!"
      redirect_to new_user_path
    end
- end
- def show
-   @user = User.all
- end
+  end
+  def show
+    @appointment = Appointment.where(user_id: current_user)
+  end
 
   def index
-    redirect_to new_session_path
   end
 
   def destroy
